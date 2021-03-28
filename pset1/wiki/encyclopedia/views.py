227cbb880b67b40f7f3e8ab323fd25b130c928b7
Arshad Alam <aalam22@students.d125.org>
2021-03-28 12:35:23 -0500

    Entry, index, and search

--- pset1/wiki/encyclopedia/views.py
+++ pset1/wiki/encyclopedia/views.py
@@ -1,10 +1,52 @@
 from django.shortcuts import render
-
 from . import util
+from django.http import HttpResponse
+import markdown2
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+class NewTaskForm(forms.Form):
+	title = forms.CharField(label="", widget=forms.TextInput(attrs={"class": "search", "placeholder": "Search Encyclopedia"}))
 
 def index(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            newTitle = findMatching(title)
+            if newTitle != None:
+                return HttpResponseRedirect(reverse("entry", kwargs={"title": newTitle}))
+            return render(request, "encyclopedia/search.html", {
+                "title": title,
+                "entries": generateResults(title),
+                "form": NewTaskForm()
+            })
+            
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": NewTaskForm()
     })
 
+def entry(request, title):
+    contents = util.get_entry(title)
+    if contents == None:
+        return HttpResponse("Page not found")
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "contents": markdown2.markdown(contents),
+        "form": NewTaskForm()
+    })
+
+def findMatching(title):
+    for entry in util.list_entries():
+        if title.upper() == entry.upper():
+            return entry
+    return None
+
+def generateResults(query):
+    results = []
+    for entry in util.list_entries():
+        if query.upper() in entry.upper():
+            results.append(entry)
+    return results
